271900fdf2382da62cf0b4455537718a25825dc6
upesnavneet <kumarnavneet7765@gmail.com>
2026-06-24 01:20:15 +0530

    changes

--- DSA/Navneet_590012555/Day10/Q2.java
+++ DSA/Navneet_590012555/Day10/Q2.java
@@ -1,14 +1,10 @@
-import java.util.*;
-
-class Q2 {
-    public static void main(String[] args) {
-        String s = "abc";
+class Solution {
+    public static String check(String s) {
         Set<Character> set = new HashSet<>();
 
         for (char c : s.toCharArray())
             set.add(c);
 
-        System.out.println(set.size() % 2 == 0 ?
-                "CHAT WITH HER!" : "IGNORE HIM!");
+        return set.size() % 2 == 0 ? "CHAT WITH HER!": "IGNORE HIM!";
     }
 }
